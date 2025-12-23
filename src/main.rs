e6b8ad45b2cefed79ff659334e3ba4eb36e7d25f
Tahar Meijs <tntmeijs@gmail.com>
2025-12-23 13:46:57 +0100

    feat: add reqwest and send test request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::header::USER_AGENT;
+use std::error::Error;
+
+const SCOUT_USER_AGENT: &str = "Scout/0.1";
+const TARGET_URL: &str = "https://tahar.dev";
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let client = reqwest::blocking::Client::new();
+    let raw_html = client
+        .get(TARGET_URL)
+        .header(USER_AGENT, SCOUT_USER_AGENT)
+        .send()?
+        .text()?;
+
+    println!("{:#?}", raw_html);
+
+    Ok(())
 }
